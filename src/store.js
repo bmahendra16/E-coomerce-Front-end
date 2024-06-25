a6daf39628dda5db49164127451518b22e34507d
bmahendra16 <mbohara991@gmail.com>
2024-06-25 23:25:29 +0545

    change for addToCart

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from './features/cart/cartSlice';
 
-export const store = configureStore({
+const store = configureStore({
     reducer: {
         cart: cartReducer,
     },
